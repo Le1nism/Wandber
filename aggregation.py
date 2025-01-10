6f5ee212495e2642da29fe88257185ac6e87c7b2
QwertyJacob <jesusfcevallos@gmail.com>
2025-01-10 12:18:20 +0000

    fed yogi working

--- aggregation.py
+++ aggregation.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import torch
 
 def federated_averaging(global_model_state_dict, participant_models_state_dict):
     """
@@ -15,39 +15,39 @@ def federated_averaging(global_model_state_dict, participant_models_state_dict):
     return averaged_state_dict
 
 
-def fed_yogi(global_model_state_dict, participant_models, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-3):
-    moment_1 = None
-    moment_2 = None
-    step = 0
+def fed_yogi(global_model_state_dict, participant_models_state_dicts, **kwargs):
 
-    num_participants = len(participant_models)
+    learning_rate = kwargs.get('learning_rate', 0.001)
+    beta1 = kwargs.get('yogi_beta1', 0.9)
+    beta2 = kwargs.get('yogi_beta2', 0.999)
+    epsilon = kwargs.get('yogi_epsilon', 1e-3)
+
+
+    num_participants = len(participant_models_state_dicts)
     if num_participants == 0:
         raise ValueError("No participant models for averaging.")
 
-    sum_gradients = [np.zeros_like(weights) for weights in global_model_state_dict]
+    sum_gradients = {}
+    for key, value in global_model_state_dict.items():
+        sum_gradients[key] = torch.zeros_like(value)
 
-    for participant_model in participant_models:
-        participant_weights = participant_model.get_weights()  # Assicurati che sia un modello e non un array
-        global_weights = global_model_state_dict
-        gradients = [participant_weight - global_weight for participant_weight, global_weight in zip(participant_weights, global_weights)]
-        for i, layer_grad in enumerate(gradients):
-            sum_gradients[i] += layer_grad
 
-    averaged_gradients = [sum_grad / num_participants for sum_grad in sum_gradients]
+    for participant_model_state_dict in participant_models_state_dicts:
+        for key, value in participant_model_state_dict.items():
+            sum_gradients[key] += (value - global_model_state_dict[key]) / num_participants
 
-    if moment_1 is None:
-        moment_1 = [np.zeros_like(weights) for weights in global_model_state_dict]
-    if moment_2 is None:
-        moment_2 = [np.zeros_like(weights) for weights in global_model_state_dict]
+    
+    moment_1 = {key: torch.zeros_like(value) for key, value in global_model_state_dict.items()}
+    moment_2 = {key: torch.zeros_like(value) for key, value in global_model_state_dict.items()}
 
-    for i, grad in enumerate(averaged_gradients):
-        step += 1
-        moment_1[i] = beta1 * moment_1[i] + (1 - beta1) * grad
-        moment_2[i] += (1 - beta2) * (grad ** 2 - moment_2[i])
+    for step, (key, grad) in enumerate(sum_gradients.items()):
+        
+        moment_1[key] = beta1 * moment_1[key] + (1 - beta1) * grad
+        moment_2[key] += (1 - beta2) * (grad ** 2 - moment_2[key])
 
-        m_hat = moment_1[i] / (1 - beta1 ** step)
-        v_hat = moment_2[i] / (1 - beta2 ** step)
+        m_hat = moment_1[key] / (1 - beta1 ** step)
+        v_hat = moment_2[key] / (1 - beta2 ** step)
 
-        global_model_state_dict[i] -= learning_rate * m_hat / (np.sqrt(v_hat) + epsilon)
+        global_model_state_dict[key] -= learning_rate * m_hat / (torch.sqrt(v_hat) + epsilon)
 
     return global_model_state_dict
